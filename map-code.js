2a1e654ab24fdec657a7dec26a397b34a1b93ec1
Benjamin Dunlap <bdunlap@tacalumniDrupal.starkart.local>
2010-09-15 17:59:24 -0700

    Incorporate a basic map with starter KML exported from "my" Gallup BID map. //bdunlap

--- map-code.js
+++ map-code.js
@@ -1,32 +1,12 @@
-  // Load the latest version of the Google Data JavaScript Client
-  google.load('gdata', '2.x', {packages: ['maps']});
-
-
-  var service = null;
-
-  function showMapData() {
-
-		var token = google.accounts.user.checkLogin('http://maps.google.com/maps/feeds');
-		if (!token) {
-			token = google.accounts.user.login('http://maps.google.com/maps/feeds');
-		}
-		service = new google.gdata.maps.MapsService('bid-poc');
-		var mapFeedUrl = 'http://maps.google.com/maps/feeds/maps/default/owned';
-
-	    service.getMapFeed(mapFeedUrl, function(feedRoot) {
-			var feed = feedRoot.feed;
-			var entries = feed.getEntries();
-			var finalText = "";
-			for (var i = 0; i < entries.length; i++) {
-				var entry = entries[i];
-				var mapTitle = entry.getTitle().getText();
-				finalText += mapTitle + "\n";
-			}
-
-			$('#output').val(finalText);
-		});
-  }
-
-  function checkLogin() {
-      return google.accounts.user.checkLogin('http://maps.google.com/maps/feeds');
-  }
+function renderMap() {
+	var bidCenter = new google.maps.LatLng(35.526000, -108.743200);
+	var myOptions = {
+		center: bidCenter,
+		mapTypeId: google.maps.MapTypeId.SATELLITE
+	}
+	 
+	var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+	   
+	var ctaLayer = new google.maps.KmlLayer('http://gallupbid.digitalcraftworks.com/bid.kml');
+	ctaLayer.setMap(map);
+}
