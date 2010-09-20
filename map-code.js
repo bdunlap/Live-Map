6d0180fdea08a8c831b8bac547a5a7e626cd613a
Liam Collins <1mailliam1@gmail.com>
2010-09-20 16:37:33 -0700

    Modified getNext() so that it returns an object instead of a URL. Also experimented with modifying maps.

--- map-code.js
+++ map-code.js
@@ -1,12 +1,37 @@
+var map;
+var marker;
+var bidCenter = new google.maps.LatLng(35.526000, -108.743200);
+var districtOutlineAndMarkers;
+
 function renderMap() {
-	var bidCenter = new google.maps.LatLng(35.526000, -108.743200);
 	var myOptions = {
 		center: bidCenter,
-		mapTypeId: google.maps.MapTypeId.SATELLITE
+		mapTypeId: google.maps.MapTypeId.SATELLITE,
+        zoom: 20
 	}
 	 
-	var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
-	   
-	var districtOutlineAndMarkers = new google.maps.KmlLayer('http://gallupbid.digitalcraftworks.com/bid.kml');
+	map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+
+    setTimeout("makeMarker()", 4000);
+
+	districtOutlineAndMarkers = new google.maps.KmlLayer('http://gallupbid.digitalcraftworks.com/bid.kml');
 	districtOutlineAndMarkers.setMap(map);
 }
+
+function makeMarker() {
+    marker = new google.maps.Marker({
+            position: bidCenter, 
+            map: map,
+            title:"Hello World!"
+        });
+
+setTimeout("manipulateMarker()", 9000);
+}
+
+function manipulateMarker() 
+{
+marker.setVisible(false);
+districtOutlineAndMarkers.setMap(map);
+}
+
+
