932c85547492123e3bfb43195c4149299d7f83a1
Liam Collins <1mailliam1@gmail.com>
2010-09-23 16:42:51 -0700

    First cut twitgoo poller.

---
+++ TwitgooPoller.php
@@ -0,0 +1,64 @@
+<?php
+
+//TODO temporary
+$twitterAccounts = array("bidPrototype");
+
+$photos = array();
+
+foreach ($twitterAccounts as $account) {
+    $twitterResponse = getPhotoResponse($account); 
+    $photoArrayForAccount = parseResponse($twitterResponse, $account); 
+    $photos = array_merge($photos, $photoArrayForAccount);
+}
+
+var_dump($photos);
+
+function getPhotoResponse($account)
+{
+    $url = 'http://twitgoo.com/api/user/timeline/' . $account;
+    $response = file_get_contents($url);
+    return $response;
+}
+
+function parseResponse($response, $account) 
+{
+    $xmlobject = simplexml_load_string($response);
+
+    //TODO check response status(?)
+
+    $images = array();
+
+    foreach ($xmlobject->children() as $media) {
+        //get text and URL
+        $text = strval($media->text); 
+        $imageUrl = strval($media->imageurl);
+
+        $imgObj = new Photo();
+
+        //TODO use getters and setters here?
+        $imgObj->text = $text;
+        $imgObj->twitterAccount = $account;
+        $imgObj->url = $imageUrl;
+
+        $images[] = $imgObj;
+    }
+    return $images;
+}
+
+
+
+class Photo {
+    public $text;
+    public $rawData;
+    public $twitterAccount;
+    public $url;
+    
+    public function getImage()
+    {
+        $imageData = file_get_contents($this->url);
+        return $imageData;
+    }
+}
+
+
+?>
