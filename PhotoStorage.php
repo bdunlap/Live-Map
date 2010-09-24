1213c0f05cbd4aa0e6fb73a5f000611403215de4
Benjamin Dunlap <bdunlap@bdunlap-laptop.localnet>
2010-09-23 17:48:12 -0700

    Define photo-storage API (very basic). //bdunlap

---
+++ PhotoStorage.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package Live-Map
+ *
+ * @class PhotoStorage
+ */
+
+class PhotoStorage
+{
+    /**
+     * @param $photo Photo
+     */
+    static public function addPhoto($photo)
+    {
+        self::_markAsSeen($id);
+    }
+
+    /**
+     * @return Photo
+     */
+    static public function getNextUnseen()
+    {
+    }
+
+    static private function _markAsSeen($id)
+    {
+    }
+}
+?>
