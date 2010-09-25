9adb206f44db7ab4b7cc3bd4eec8837c78dcbbd5
Liam Collins <1mailliam1@gmail.com>
2010-09-25 14:22:25 -0700

    added graceful handling of EMPTY and FAIL cases of getNext().

--- js/gallery.js
+++ js/gallery.js
@@ -20,19 +20,28 @@ function getNext()
 
     var parsedImg = $.parseJSON(Img);
     //check parsed image status
-    if(parsedImg.status == "FAIL") {
+    
+    if(parsedImg.status === "FAIL") {
         //do something with failed response
+        $('#server-status').text(parsedImg.status);
 
-    } else if (parsedImg.status == "EMPTY") {
+        return null;
+
+    } else if (parsedImg.status === "EMPTY") {
         //do something with empty response
+        $('#server-status').text(parsedImg.status);
 
-    } else if (parsedImg.status == "SUCCESS") {
+        return null;
+
+    } else if (parsedImg.status === "SUCCESS") {
         //response is good
+        $('#server-status').text(parsedImg.status);
 
         console.log(parsedImg);
 
         return parsedImg;
     }
+
 }
 
 var imageCounter = 0;
@@ -66,8 +75,8 @@ function doTimer()
 function placeNext()
 {
 	var newImgObject = getNext();
-	if (newImgObject == null) {
-		console.log("All photos in database are currently being displayed.");
+	if (newImgObject === null) {
+		console.log("getNext() returned null.");
 		return;
     }
     var newUrl = newImgObject.url;
