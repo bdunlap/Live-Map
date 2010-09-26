657d59587a4233a838d5d711c0687f86d2cbc883
Benjamin Dunlap <bdunlap@bdunlap-laptop.localnet>
2010-09-26 09:24:08 -0700

    Finish markers. //bdunlap

--- js/map-code.js
+++ js/map-code.js
@@ -56,90 +56,58 @@ function makeMarkers(map)
 				position: new google.maps.LatLng(35.529345,-108.740294),
 				map: map,
 				title:"Mission G: Painted Horse Sculpture"
-			})
-			/*
+			}),
+
 		Mission_F2_Code_Talkers_Statue:
+            new google.maps.Marker({
+                position: new google.maps.LatLng(35.529179,-108.74035),
+                map: map,
+                title:"Mission F2: Code Talkers Statue"
+            }),
+
 		Mission_F1_Chief_Manuelito_Statue:
+            new google.maps.Marker({
+                position: new google.maps.LatLng(35.529138,-108.740535),
+                map: map,
+                title:"Mission F1: Chief Manuelito Statue"
+            }),
+
 		Mission_A2_First_American_Traders:
+            new google.maps.Marker({
+                position: new google.maps.LatLng(35.528454,-108.741005),
+                map: map,
+                title:"Mission A2: First American Traders"
+            }),
+
 		Womens_MultiCultural_Mural:
+            new google.maps.Marker({
+                position: new google.maps.LatLng(35.526375,-108.742783),
+                map: map,
+                title:"Women's Multi-Cultural Mural"
+            }),
+
 		Zuni_Mural:
+            marker9 = new google.maps.Marker({
+                position: new google.maps.LatLng(35.525433,-108.741928),
+                map: map,
+                title:"Zuni Mural"
+            }),
+
 		Veterans_Mural:
+            new google.maps.Marker({
+                position: new google.maps.LatLng(35.525311,-108.743103),
+                map: map,
+                title:"Veteran's Mural"
+            }),
+
 		Long_Walk_Home_Mural:
-	*/
+            new google.maps.Marker({
+                position: new google.maps.LatLng(35.525013,-108.743729),
+                map: map,
+                title:"Long Walk Home Mural"
+            })
 	};
 
 	return markerList;
-
-    markerCenter = new google.maps.LatLng(35.528454,-108.741005);
-    marker3 = new google.maps.Marker({
-            position: markerCenter,
-            map: map,
-            title:"Mission A2: First American Traders"
-        });
-      markerCenter = new google.maps.LatLng(35.529138,-108.740535);
-      marker4 = new google.maps.Marker({
-            position: markerCenter,
-            map: map,
-            title:"Mission F1: Chief Manuelito Statue"
-        });
-      markerCenter = new google.maps.LatLng(35.529179,-108.74035);
-      marker4a = new google.maps.Marker({
-            position: markerCenter,
-            map: map,
-            title:"Mission F2: Code Talkers Statue"
-        });
-    var infoWindow = new google.maps.InfoWindow({
-        content:'<i>Paul Newman, Artist</i><div>"...Most of the history of a community takes place in the residents\' daily lives, for instance, where they work and, importantly, their environment. &nbsp;We chose to emphasize five broad themes - the landscape, railroads, Highway 66, rodeos, western life and coal mining."</div>'
-        });
-      markerCenter = new google.maps.LatLng(35.525433,-108.741928);
-       marker9 = new google.maps.Marker({
-            position: markerCenter,
-            map: map,
-            title:"Zuni Mural"
-        });
-    var infoWindow = new google.maps.InfoWindow({
-        contents:'<i>Geddy Epaloose, Artist</i><div>"The story line reads from south to north like the road from Zuni to Gallup and shows how Zunis from their world and their beginnings have brought Zuni traditions, culture, religion and history into this new world and Gallup..."</div>'
-        });
-      markerCenter = new google.maps.LatLng(35.525013,-108.743729);
-        marker11 = new google.maps.Marker({
-            position: markerCenter,
-            map: map,
-            title:"Long Walk Home Mural"
-        });
-    var infoWindow = new google.maps.InfoWindow({
-        contents:'<i>Richard K. Yazzie, Artist</i><div>In 1864... thousands of Navajo people were forcibly marched from Canyon de Chelly... to Fort Sumner four hundred miles away. &nbsp;Eventually 7,000 Navajo were imprisoned there. &nbsp;The captives suffered four yeas of deplorable conditions... until it no longer became feasible to hold them. &nbsp;The Peace Treaty of 1868 was signed and the Navajo were released.</div>'
-        });
-      markerCenter = new google.maps.LatLng(35.525311,-108.743103);
-        marker12 = new google.maps.Marker({
-            position: markerCenter,
-            map: map,
-            title:"Veteran's Mural"
-        });
-    var infoWindow = new google.maps.InfoWindow({
-        contents:'<i>Jerry Brown, Artist</i>'
-        });
-      markerCenter = new google.maps.LatLng(35.526375,-108.742783);
-     marker13 = new google.maps.Marker({
-            position: markerCenter,
-            map: map,
-            title:"Women's Multi-Cultural Mural"
-        });
-    var infoWindow = new google.maps.InfoWindow({
-        contents:'<i>Erica Rae Sykes, Artist</i><div>We offer a tribute to the women who have carried on the traditions of daily life of their own cultures...The large symbolic storyteller at the left of the mural pays homage to women who have kept multi-cultural memories alive by telling stories of the past.</div>'
-        });
-      markerCenter = new google.maps.LatLng(35.526604,-108.743256);
-     marker15 = new google.maps.Marker({
-            position: markerCenter,
-            map: map,
-            title:"Coal Mining Era Mural"
-        });
-    var infoWindow = new google.maps.InfoWindow({
-        contents:'<i>Andrew Butler, Artist</i><div>The 1935 Gallup Riot took place along this alley during which Sheriff Carmichael was killed. &nbsp;Depicted are a Navajo rail crew, legendary for their speed, the McKinley strip mine on the Navajo Reservation, the Gamerco Electric Generating Plant, coal competitions on days off, and the McGaffey timber industry nearby supplying the mines with structural materials.</div>>'
-        });
-    var infoWindow = new google.maps.InfoWindow({
-        contents:'<i>Be Sargent, Artist</i><div>"The first 29 Code Talkers devised the Navajo Code. &nbsp;Part of the code was an alphabet which was used to spell words like names of strategic places. &nbsp;The mural depicts the code being passed on to the children of the Code Talkers symbolizing the perpetuation of the Navajo language."</div>'
-        });
-
-
 }
 
